d96915219461ac688e5556b88d82f3aa977fca87
Arun Kumar Chauhan <arun8987236915@gmail.com>
2024-07-28 17:00:03 +0530

    my first commit

---
+++ force-app/main/default/lwc/firstKumarLightningWebComponent/firstKumarLightningWebComponent.js
@@ -0,0 +1,8 @@
+import { LightningElement } from 'lwc';
+
+export default class FirstKumarLightningWebComponent extends LightningElement {
+  name = 'Arun Kumar Chauhan';
+  company = 'Seven Alpha';
+  designation = 'Founder';
+  salary = '$200000';
+}
---
+++ force-app/main/default/lwc/firstLwcProgramme/firstLwcProgramme.js
@@ -0,0 +1,8 @@
+import { LightningElement } from 'lwc';
+
+export default class FirstLwcProgramme extends LightningElement {
+  name = 'Arun Kumar Chauhan';
+  company = 'Seven Gama';
+  designation = 'Founder';
+  salary = '$200';
+}
